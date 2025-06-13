df93ced6d93df4c7ae9d71d9b5574214edca41ff
Firsandi andraw Febriansyah <firsandiandra@gmail.com>
2025-06-13 21:41:53 +0700

    Kelola Guide

---
+++ Firsandi_Travella/Presenter/GuidePresenter.cs
@@ -0,0 +1,41 @@
+﻿using Firsandi_Travella.Database;
+using Firsandi_Travella.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firsandi_Travella.Interfaces;
+
+namespace Firsandi_Travella.Presenter
+{
+           public class GuidePresenter
+        {
+            private readonly IGuideView _view;
+            private readonly GuideRepository _repository;
+
+            public GuidePresenter(IGuideView view)
+            {
+                _view = view;
+                _repository = new GuideRepository();
+            }
+
+            public void LoadGuides()
+            {
+                var guides = _repository.GetGuides();
+                _view.ShowGuides(guides);
+            }
+
+            public void UpdateGuide(GuideModels guide)
+            {
+                _repository.UpdateGuide(guide);
+                LoadGuides(); // 🔥 Perbarui tampilan setelah update
+            }
+
+            public void DeleteGuide(int id)
+            {
+                _repository.DeleteGuide(id);
+                LoadGuides(); // 🔥 Perbarui tampilan setelah hapus
+            }
+        }
+}
---
+++ Firsandi_Travella/Views/V_Admin/V_EditGuide.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Firsandi_Travella.Views.V_Admin
+{
+    public partial class V_EditGuide : Form
+    {
+        public V_EditGuide()
+        {
+            InitializeComponent();
+        }
+    }
+}
